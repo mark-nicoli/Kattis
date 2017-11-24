0e9ffaf3725b515d901311d027dcfd2a5ad284a0
Mark Nicoli <mark21198@gmail.com>
2017-11-24 10:42:28 +0000

    Create difference.java

---
+++ difference.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class difference
+{
+    public static void main(String[]args)
+    {
+        Scanner scan = new Scanner(System.in);
+        for(int i = 0;i<2;i++)
+        {
+            for(int j= 0;j<2;j++)
+            {
+
+            }
+        }
+    }
+}
