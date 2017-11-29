1a1516872455ddfa24a8c46cf13de72b5df4fffc
Mark Nicoli <mark21198@gmail.com>
2017-11-29 13:41:15 +0000

    Add files via upload

---
+++ spavanac.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class spavanac {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int h = scan.nextInt();
+        int m = scan.nextInt();
+        if(m < 45)
+        {
+            if(h == 0) h = 23;
+            else h--;
+            m = 60 - (45 - m);
+        }
+        else m -= 45;
+        System.out.println(h+" "+m);
+    }
+}
