951aa15f020441657b9e3261c1667295216532c6
Mark Nicoli <mark21198@gmail.com>
2017-12-21 16:16:07 +0000

    Update soylent.java

--- soylent.java
+++ soylent.java
@@ -5,15 +5,13 @@ public class soylent
         Scanner scan = new Scanner(System.in);
         int size = scan.nextInt();
         int array[] = new int[size];
-        double sum = 0;
-        for(int i = 0;i<size;i++)
-        {
-            array[i]= scan.nextInt();
-        }
-        for(int i = 0;i<size;i++)
-        {
+        int sum ;
+        for(int i = 0;i<size;i++) array[i]= scan.nextInt();
+        for(int i = 0;i<size;i++) {
             sum = array[i]/400;
-            System.out.println(sum);
+            if(array[i]%400==0) System.out.println(sum);
+            else System.out.println(sum+1);
         }
     }
 }
+
