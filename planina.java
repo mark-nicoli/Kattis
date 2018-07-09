debd1df517aadaacd7fb8e6f09d1c5b33b259962
Mark Nicoli <mark21198@gmail.com>
2018-07-09 15:55:40 +0100

    Update planina.java

--- planina.java
+++ planina.java
@@ -3,10 +3,8 @@ public class planina
 {
   public static void main(String[]args){
     Scanner scan = new Scanner(System.in);
-    int in= scan.nextInt();
-    double b = 2.0;
-    double a = Math.pow(b ,in);
-    int whatever = (int)Math.pow((a+1),b);
-    System.out.println(whatever);
+    int iteration = scan.nextInt();
+    System.out.println((int)Math.pow((Math.pow(2 ,iteration)+1),2));
   }
 }
+
